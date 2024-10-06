8797f5fd36018b78e5a12ac84ac6945cb5a88c97
n0xena <68239493+n0xena@users.noreply.github.com>
2024-10-06 09:36:36 +0200

    update to zfs-2.3.0-rc1
    update conf.sh to pull zfs-2.3.0-rc1

--- conf.sh
+++ conf.sh
@@ -2,11 +2,11 @@
 #
 # FIXME: reset all kernel configs set to pkgrel=1 when this changes
 #
-openzfs_version="2.2.6"
+openzfs_version="zfs-2.3.0-rc1"
 openzfs_rc_version="2.1.0-rc8"
 
 # The OpenZFS source hashes are from github.com/openzfs/zfs/releases
-zfs_src_hash="c92e02103ac5dd77bf01d7209eabdca55c7b3356aa747bb2357ec4222652a2a7"
+zfs_src_hash="a74b54788640629b426bd3ab3f2484e62c4620e899c92096bc8ef5fed1360a0a"
 zfs_rc_src_hash="8627702ac841d38d5211001c76937e4097719c268b110e8836c0da195618fad2"
 
 zfs_initcpio_install_hash="d19476c6a599ebe3415680b908412c8f19315246637b3a61e811e2e0961aea78"
