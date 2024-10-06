6f44762cad6644d72e044b2964826c94c33b8400
n0xena <68239493+n0xena@users.noreply.github.com>
2024-10-06 10:05:41 +0200

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -2,7 +2,7 @@
 #
 # FIXME: reset all kernel configs set to pkgrel=1 when this changes
 #
-openzfs_version="zfs-2.3.0-rc1"
+openzfs_version="2.3.0-rc1"
 openzfs_rc_version="2.1.0-rc8"
 
 # The OpenZFS source hashes are from github.com/openzfs/zfs/releases
