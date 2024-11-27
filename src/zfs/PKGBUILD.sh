06bb275dca1b794edf14aed2dfce4c6879583554
n0xena <68239493+n0xena@users.noreply.github.com>
2024-11-27 01:46:55 +0100

    override for kernel version enforcement to build 6.12
    currently zfs-2.3 is only up to 6.11 - hence need override to build for 6.12

--- src/zfs/PKGBUILD.sh
+++ src/zfs/PKGBUILD.sh
@@ -34,7 +34,8 @@ build() {
                 --datadir=/usr/share --includedir=/usr/include --with-udevdir=/usr/lib/udev \\
                 --libexecdir=/usr/lib --with-config=kernel \\
                 --with-linux=/usr/lib/modules/\${_extramodules}/build \\
-                --with-linux-obj=/usr/lib/modules/\${_extramodules}/build
+                --with-linux-obj=/usr/lib/modules/\${_extramodules}/build \\
+                --enable-linux-experimental
     make
 }
 
