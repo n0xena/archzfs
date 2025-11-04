fdec432591b1e17be48c8e11d9cb14a8809c2212
n0xena <68239493+n0xena@users.noreply.github.com>
2025-11-04 08:25:46 +0100

    Update dkms.sh
    remove rc_path

--- src/kernels/dkms.sh
+++ src/kernels/dkms.sh
@@ -41,7 +41,6 @@ update_dkms_rc_pkgbuilds() {
     pkg_list=("zfs-dkms-rc")
     archzfs_package_group="archzfs-dkms-rc"
     zfs_pkgver=${openzfs_rc_version/-/_}
-    zfs_rc_path=${openzfs_rc_version}
     zfs_mod_ver="\${pkgver}"
     zfs_pkgrel=${pkgrel_rc}
     zfs_pkgname="zfs-dkms-rc"
@@ -50,7 +49,7 @@ update_dkms_rc_pkgbuilds() {
     # Paths are relative to build.sh
     zfs_dkms_pkgbuild_path="packages/${kernel_name}/${zfs_pkgname}"
     zfs_src_target="https://github.com/openzfs/zfs/releases/download/zfs-\${pkgver/_/-}/zfs-\${pkgver/_/-}.tar.gz"
-    zfs_workdir="\${srcdir}/zfs-\${rc_path}"
+    zfs_workdir="\${srcdir}/zfs-\${openzfs_rc_version}"
 }
 
 #update_dkms_git_pkgbuilds() {
